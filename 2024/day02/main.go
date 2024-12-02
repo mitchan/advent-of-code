5d75ae10b0d413eb5bdbb05048248d9b7c9fc25a
Francesco Casetta <francesco.casetta@gmail.com>
2024-12-02 16:17:29 +0100

    2024: day02 - part2

--- 2024/day02/main.go
+++ 2024/day02/main.go
@@ -22,6 +22,12 @@ type Mode int
 
 func main() {
 	rows := strings.Split(input, "\n")
+
+	part1(rows)
+	part2(rows)
+}
+
+func part1(rows []string) {
 	validRowsCount := 0
 
 	for _, row := range rows {
@@ -33,16 +39,16 @@ func main() {
 
 		valid := true
 		mode := Idle
-		var lastNum int64 = 0
+		var lastNum int64 = -1
 
-		for index, value := range values {
+		for _, value := range values {
 			// parse number
 			n, err := strconv.ParseInt(value, 10, 64)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			if index == 0 {
+			if lastNum == -1 {
 				lastNum = n
 				continue
 			}
@@ -77,5 +83,71 @@ func main() {
 		}
 	}
 
-	fmt.Println("The count of valid rows is: ", validRowsCount)
+	fmt.Println("Part1 - The count of valid rows is: ", validRowsCount)
+}
+
+func part2(rows []string) {
+	validRowsCount := 0
+
+	for _, row := range rows {
+		values := strings.Fields(row)
+
+		if len(values) == 0 {
+			continue
+		}
+
+		for skip := range values {
+			valid := true
+			mode := Idle
+			var lastNum int64 = -1
+
+			for index, value := range values {
+				if index == skip {
+					continue
+				}
+
+				// parse number
+				n, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if lastNum == -1 {
+					lastNum = n
+					continue
+				}
+
+				diff := lastNum - n
+				lastNum = n
+
+				if diff == 0 || diff > 3 || diff < -3 {
+					valid = false
+					break
+				}
+
+				if diff < 0 {
+					if mode == Idle {
+						mode = Decreasing
+					} else if mode == Increasing {
+						valid = false
+						break
+					}
+				} else {
+					if mode == Idle {
+						mode = Increasing
+					} else if mode == Decreasing {
+						valid = false
+						break
+					}
+				}
+			}
+
+			if valid {
+				validRowsCount += 1
+				break
+			}
+		}
+	}
+
+	fmt.Println("Part2 - The count of valid rows is: ", validRowsCount)
 }
